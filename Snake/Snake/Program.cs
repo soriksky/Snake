6c3763b4b059b13c36aac78b3cc93a8f06f106f8
Sergey Chornyi <soriksky@gmail.com>
2016-06-24 14:51:10 +0300

    Add new class Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,6 +10,13 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
+            p1.x = 3;
+            p1.y = 4;
+            p1.sym = '#';
+            p1.Draw();
+
+            Console.ReadLine();
         }
     }
 }
