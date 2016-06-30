524350a1f017990977a4a0c623099f41c07720ba
Sergey Chornyi <soriksky@gmail.com>
2016-06-30 15:30:08 +0300

    Add List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,6 +13,8 @@ namespace Snake
             Point p1 = new Point(3,4,'#');
             p1.Draw();
 
+            List < Point > listPoint = new List<Point>();
+            listPoint.Add(p1);
             Console.ReadLine();
         }
     }
