05dadaaa5e61a3840ee50a934197d0fb49865dec
Sergey Chornyi <soriksky@gmail.com>
2016-06-30 15:32:28 +0300

    Add list

---
+++ Snake.git/Snake/Snake/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point()
+        {
+
+        }
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
---
+++ Snake.git/Snake/Snake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(3,4,'#');
+            p1.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
